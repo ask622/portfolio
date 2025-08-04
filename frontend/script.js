52e2425af625290e98f64231f187aaaea9f957d4
AAKASH <aakash197516@gmail.com>
2025-08-04 23:31:58 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     try {
-      const response = await fetch('https://portfolio-8eze.onrender.com/', {
+      const response = await fetch('https://portfolio-8eze.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -92,4 +92,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
 
